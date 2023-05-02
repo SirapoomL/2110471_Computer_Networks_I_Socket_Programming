180d2a58cc12445a543e161167180d6d82366f8c
AkiraSitdhi <ppoonpy@gmail.com>
2023-05-03 00:25:07 +0700

    styled user list

--- frontend/src/components/chatRoom/userList/UserListContainer.tsx
+++ frontend/src/components/chatRoom/userList/UserListContainer.tsx
@@ -4,7 +4,7 @@ import { useSocket } from "../../SocketProvider"
 import { UserInterface } from "../../../interfaces/UserInterface";
 import { useUser } from "../../UserProvider";
 import { styled } from "@mui/material/styles";
-import { Avatar, List, ListItem, ListItemAvatar, ListItemText, Typography } from "@mui/material";
+import { Avatar, List, ListItem, ListItemAvatar, ListItemText} from "@mui/material";
 
 import "../../../pages/component.css"
 import { LIGHTCOLOR, DARKCOLOR } from "../../../pages/theme";
@@ -44,13 +44,13 @@ export function UserListContainer() {
       <div style={{color:getTheme().text}}>
         <h2>{users?.length} User(s) Online</h2>
       </div>
-      <List sx={{ paddingTop:1 }}>
+      <List sx={{width:"100%"}} >
         {users?.map((user, idx) => (
-          <ListItem key={idx} button onClick={() => handleClick(user)} disablePadding sx={{ borderBottom: "3px solid #ddd", borderRadius: '10px', '&:hover': { backgroundColor: '#E5E7EB' } }}>
-            <ListItemAvatar sx={{paddingTop: 2, paddingBottom: 1}}>
+          <ListItem key={idx} button onClick={() => handleClick(user)} disablePadding sx={{width:"100%",marginTop:"1px", '&:hover': {background:getTheme().lighter} }}>
+            <ListItemAvatar sx={{marginLeft:"20%",marginRight:"5%",marginBottom:"10px",paddingTop:1.5}}>
               <Avatar alt="avatar" src={avatars_url[user.avatar ?? 0]} sx={{ width: 40, height: 40 }} />
             </ListItemAvatar>
-            <ListItemText primary={user.name} primaryTypographyProps={{ sx: { textOverflow: "ellipsis", overflow: "hidden", whiteSpace: "nowrap" } }} />
+            <ListItemText primary={user.name} primaryTypographyProps={{ sx: { paddingRight:"3%",color:getTheme().text, textOverflow: "ellipsis", overflow: "hidden", whiteSpace: "nowrap" } }} />
           </ListItem>
         ))}
       </List>

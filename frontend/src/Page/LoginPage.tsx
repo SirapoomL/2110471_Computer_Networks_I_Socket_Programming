7cfe5ccd44d1e2881ceaa343db2d669aaad264ba
sirapoom leesuravanich <sirapoomlee@gmail.com>
2023-05-03 03:45:11 +0700

    config port

--- frontend/src/Page/LoginPage.tsx
+++ frontend/src/Page/LoginPage.tsx
@@ -4,14 +4,7 @@ import { Login } from "../components/pages/Login";
 
 export default function LoginPage() {
     useEffect(() => {
-        let serverUrl = sessionStorage.getItem('serverUrl');
-        if(serverUrl) return;
-        serverUrl =  prompt('enter websocket service server (default: localhost:2001)', '');
-        while(!serverUrl || serverUrl === ''){
-            serverUrl = 'http://localhost:2001'
-            // serverUrl =  prompt('enter websocket server', '');
-        }
-        sessionStorage.setItem('serverUrl', serverUrl);
+        sessionStorage.setItem('serverUrl', 'http://localhost:6789');
     })
     return (
         <>

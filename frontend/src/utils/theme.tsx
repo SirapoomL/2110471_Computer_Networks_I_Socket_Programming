abda12a264bc33b1f8162bc693333454cfa6445e
sirapoom leesuravanich <sirapoomlee@gmail.com>
2023-05-03 03:46:59 +0700

    add theme

---
+++ frontend/src/utils/theme.tsx
@@ -0,0 +1,15 @@
+export const LIGHTCOLOR = {
+    primary: '#fff2CC',
+    secondary: '#ffffff',
+    line: '#675D50',
+    text: '#002B5B',
+    lighter: '#FFF9E6',
+  }
+  
+export const DARKCOLOR = {
+    primary: '#23272a',
+    secondary: '#2c2f33',
+    line: '#B8621B',
+    text: '#E3CCAC',
+    lighter: '#464849',
+  }

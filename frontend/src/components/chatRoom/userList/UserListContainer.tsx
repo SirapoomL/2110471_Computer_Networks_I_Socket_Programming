2689fe9cd335002dfb19eadcf45f1aeae478e4dd
AkiraSitdhi <ppoonpy@gmail.com>
2023-05-02 23:06:05 +0700

    styled sth idk

--- frontend/src/components/chatRoom/userList/UserListContainer.tsx
+++ frontend/src/components/chatRoom/userList/UserListContainer.tsx
@@ -1,9 +1,13 @@
+import { Fragment, useEffect, useState } from "react";
 import { avatars_url } from "../../../data/Avatar";
 import { useSocket } from "../../SocketProvider"
 import { UserInterface } from "../../../interfaces/UserInterface";
 import { useUser } from "../../UserProvider";
 import { styled } from "@mui/material/styles";
 import { Avatar, List, ListItem, ListItemAvatar, ListItemText, Typography } from "@mui/material";
+
+import "../../../pages/component.css"
+import { LIGHTCOLOR, DARKCOLOR } from "../../../pages/theme";
 
 const UserListContainerWrapper = styled('div')(({ theme }) => ({
   height: "100%",
@@ -25,9 +29,19 @@ export function UserListContainer() {
       checkDm(user.id);
     }
   }
+
+  const [theme, setTheme] = useState("lighttheme");
+  const toggleTheme = () => {
+      setTheme((curr) => (curr === "lighttheme" ? "darktheme" : "lighttheme"));
+  };
+  const getTheme = () => {
+      if(theme==="lighttheme")return LIGHTCOLOR
+      return DARKCOLOR
+  }
+
   return (
     <UserListContainerWrapper>
-      <Typography variant="subtitle1" sx={{ fontSize: 14, fontWeight: 600, paddingLeft: 1 }}>
+      <Typography variant="subtitle1" sx={{ fontSize: 14, fontWeight: 600, paddingLeft: 1,color:getTheme().text}}>
         ONLINE - {users?.length}
       </Typography>
       <List sx={{ paddingTop:1 }}>

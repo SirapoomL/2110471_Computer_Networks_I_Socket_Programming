12b60310451051a037411c34741078c7403bfba4
AkiraSitdhi <ppoonpy@gmail.com>
2023-05-03 06:54:04 +0700

    sleep laew

--- frontend/src/components/chatRoom/roomList/CreateRoomContainer.tsx
+++ frontend/src/components/chatRoom/roomList/CreateRoomContainer.tsx
@@ -1,6 +1,4 @@
 import { useEffect, useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCirclePlus } from "@fortawesome/free-solid-svg-icons";
 import { useSocket } from "../../SocketProvider";
 import { LIGHTCOLOR, DARKCOLOR } from "../../../utils/theme";
 
@@ -27,7 +25,7 @@ export function CreateRoomContainer() {
   }
   return <>
   <div style={{height:"3rem", width:"100%", display:"flex", flexDirection:"row", padding:"2rem 0.5rem", alignItems:"center", marginBottom:"auto"}}>
-    <input type="text" id="new-room-name" value={roomNameInput} onChange={e=>{setRoomNameInput(e.target.value)}} placeholder="Input New Room Name" style={{ width:"0", flex:"1 1 auto", borderRadius:"6px", marginRight:"0.5rem", padding:"0.4rem 1rem"}}/>
+    <input type="text" id="new-room-name" value={roomNameInput} onChange={e=>{setRoomNameInput(e.target.value)}} placeholder="Enter room name" style={{ width:"0", flex:"1 1 auto", borderRadius:"6px", marginRight:"0.5rem", padding:"0.4rem 1rem"}}/>
     <button style={{height:"35px",background:getTheme().text,color: getTheme().primary, flex:"0 1 auto",paddingInline:"5px",borderRadius:"10px"}} onClick={onClick}>
       Create
       {/* <FontAwesomeIcon icon={faCirclePlus} size="xl" style={{color: getTheme().line, flex:"0 1 auto"}} /> */}

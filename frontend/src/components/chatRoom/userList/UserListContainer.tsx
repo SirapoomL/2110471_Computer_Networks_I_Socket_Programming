2d51a38791f3d54938aca6bb86cffe460e9adb65
sirapoom leesuravanich <sirapoomlee@gmail.com>
2023-05-03 04:45:04 +0700

    fix defualt theme bugs

--- frontend/src/components/chatRoom/userList/UserListContainer.tsx
+++ frontend/src/components/chatRoom/userList/UserListContainer.tsx
@@ -4,18 +4,15 @@ import { UserInterface } from "../../../interfaces/UserInterface";
 import { useUser } from "../../UserProvider";
 import { styled } from "@mui/material/styles";
 import { Avatar, List, ListItem, ListItemAvatar, ListItemText, Typography } from "@mui/material";
+import { LIGHTCOLOR, DARKCOLOR } from "../../../utils/theme";
 
-const UserListContainerWrapper = styled('div')(({ theme }) => ({
-  height: "100%",
-  minWidth: "17vw",
-  maxWidth: "20vw",
-  padding: "14px 9px",
-  backgroundColor: "#F3F4F6",
-  flex: "1 1 auto",
-  overflowY: "auto",
-  borderTopLeftRadius: "20px",
-  borderTopRightRadius: "20px",
-}));
+let theme = sessionStorage.getItem('theme');
+const getTheme = () => {
+  let theme = sessionStorage.getItem('theme');
+  console.log(theme)
+      if(theme?.toString()==="lighttheme")return LIGHTCOLOR
+      return DARKCOLOR
+}
 
 export function UserListContainer() {
   const { users, checkDm, socket } = useSocket();
@@ -26,20 +23,20 @@ export function UserListContainer() {
     }
   }
   return (
-    <UserListContainerWrapper>
-      <Typography variant="subtitle1" sx={{ fontSize: 14, fontWeight: 600, paddingLeft: 1 }}>
-        ONLINE - {users?.length}
-      </Typography>
-      <List sx={{ paddingTop:1 }}>
+    <div style={{width:"100%",height:"100%",display:"flex",flexDirection:"column",justifyContent:"flex-start",alignItems:"center",backgroundColor:getTheme().primary}}>
+      <div style={{color:getTheme().text,marginTop:"2rem"}}>
+        <h2>{users?.length} User(s) Online</h2>
+      </div>
+      <List sx={{width:"100%"}} >
         {users?.map((user, idx) => (
-          <ListItem key={idx} button onClick={() => handleClick(user)} disablePadding sx={{ borderBottom: "3px solid #ddd", borderRadius: '10px', '&:hover': { backgroundColor: '#E5E7EB' } }}>
-            <ListItemAvatar sx={{paddingTop: 2, paddingBottom: 1}}>
-              <Avatar alt="avatar" src={avatars_url[user.avatar ?? 0]} sx={{ width: 40, height: 40 }} />
+          <ListItem key={idx} button onClick={() => handleClick(user)} disablePadding sx={{width:"100%",marginTop:"1px", '&:hover': {background:getTheme().lighter} }}>
+            <ListItemAvatar sx={{marginLeft:"20%",marginRight:"5%",marginBottom:"10px",paddingTop:1.5}}>
+              <Avatar alt="profile" src={avatars_url[user.avatar ?? 0]} sx={{ width: 40, height: 40 }} />
             </ListItemAvatar>
-            <ListItemText primary={user.name} primaryTypographyProps={{ sx: { textOverflow: "ellipsis", overflow: "hidden", whiteSpace: "nowrap" } }} />
+            <ListItemText primary={user.name} primaryTypographyProps={{ sx: { paddingRight:"3%",color:getTheme().text, textOverflow: "ellipsis", overflow: "hidden", whiteSpace: "nowrap" } }} />
           </ListItem>
         ))}
       </List>
-    </UserListContainerWrapper>
+    </div>
   );
 }
